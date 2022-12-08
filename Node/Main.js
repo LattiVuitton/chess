63a3430d6e54d0fc21374c11e336ca75b36fb023
Latti31 <111042692+Latti31@users.noreply.github.com>
2022-12-09 01:19:20 +1100

    Allow multiple games to be run

---
+++ Node/Main.js
@@ -0,0 +1,51 @@
+var agents = require('./Agents');
+
+const agent1 = agents.getAgent("random", 1, true);
+const agent2 = agents.getAgent("random", 2, false);
+
+agent1Wins = 0
+agent2Wins = 0
+draws = 0
+
+const roundsToPlay = 3
+
+console.log("")
+
+for (let i = 0; i < roundsToPlay; i++){
+
+    var game = require('./GameRunner');
+    
+    const winner = game.runGame(agent1, agent2);
+
+    if (winner != null) {
+        result = "Player " + winner.id + " Wins";
+        if (winner.id === 1) {
+            agent1Wins += 1
+        }
+        else {
+            agent2Wins += 1
+        }
+    }
+
+    else {
+        result = "Draw";
+        draws += 1
+    }
+    
+    console.log(result);
+}
+
+const gamesPlayed = agent1Wins + agent2Wins + draws
+const roundingDecimals = 2
+
+const round = Math.pow(10, roundingDecimals)
+
+console.log("\nGames Played  :  " + gamesPlayed)
+console.log("Agent 1       :  " + agent1Wins + " (" + Math.round((100 * agent1Wins / gamesPlayed + Number.EPSILON) * round) / round + " %)")
+console.log("Agent 2       :  " + agent2Wins + " (" + Math.round((100 * agent2Wins / gamesPlayed + Number.EPSILON) * round) / round + " %)")
+console.log("Draws         :  " + draws + " (" + Math.round((100 * draws / gamesPlayed + Number.EPSILON) * round) / round + " %)")
+console.log("")
+
+
+
+
