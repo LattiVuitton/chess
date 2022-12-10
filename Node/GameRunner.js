b51523f5e5f9c1f7ce04156fe2a9e9852876a006
Latti31 <111042692+Latti31@users.noreply.github.com>
2022-12-11 00:18:40 +1100

    Printing FEN enabled

--- Node/GameRunner.js
+++ Node/GameRunner.js
@@ -2,27 +2,18 @@ const chess = require('chess.js')
 
 var game = new chess.Chess()
 
-exports.runGame = function run(agent1, agent2) {
+exports.runGame = function run(agent1, agent2, printGame) {
 
     game.reset()
 
     var round = 0
 
     while (true) {
-    
-        if (game.isGameOver()) {
-            if (game.isCheck()) {
-                return agent1
-            }
-            return null
+
+        if (printGame) {
+            console.log(game.fen())
         }
-
-        const moves1 = game.moves()
-        const move1 = agent1.selectMove(game, moves1)
-        game.move(move1)
-        
-        round++
-
+    
         if (game.isGameOver()) {
             if (game.isCheck()) {
                 return agent2
@@ -30,7 +21,40 @@ exports.runGame = function run(agent1, agent2) {
             return null
         }
 
-        const moves2 = game.moves()
+        var moves1 = null
+
+        if (agent1.requiresVerbose) {
+            moves1 = game.moves({ verbose: true })
+        }
+
+        else {
+            moves1 = game.moves()
+        }
+        const move1 = agent1.selectMove(game, moves1)
+        game.move(move1)
+        
+        round++
+
+        if (printGame) {
+            console.log(game.fen())
+        }
+
+        if (game.isGameOver()) {
+            if (game.isCheck()) {
+                return agent1
+            }
+            return null
+        }
+
+        var moves2 = null
+
+        if (agent2.requiresVerbose) {
+            moves2 = game.moves({ verbose: true })
+        }
+
+        else {
+            moves2 = game.moves()
+        }
         const move2 = agent2.selectMove(game, moves2)
         game.move(move2)
         
