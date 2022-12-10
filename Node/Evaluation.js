53e4d453f693a084c5dc0147d58e347e042610e1
Latti31 <111042692+Latti31@users.noreply.github.com>
2022-12-11 00:18:28 +1100

    Piece Value Evaluation

--- Node/Evaluation.js
+++ Node/Evaluation.js
@@ -1,3 +1,13 @@
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+const pieceValues = {
+    'p': 1,
+    'n': 3.05,
+    'b': 3.33,
+    'r': 5.63,
+    'q': 9.5,
+    'k': 0
+}
+
 exports.evaluateBoard = function evaluate(game) {
 
     // Artificial Cost
@@ -9,4 +19,30 @@ exports.evaluateBoard = function evaluate(game) {
 
     // Evaluation
     return count
+}
+
+exports.pieceValue = function countPieces(game, myColor) {
+    
+    var myScore = 0
+    var oppScore = 0
+
+    var tilesList = []
+
+    for (let i = 0; i < letters.length; i++){
+        for (let j = 1; j < letters.length + 1; j++){
+            tile = letters[i] + j
+            tilesList.push(tile)
+            piece = game.get(tile)
+            if (piece != false) {
+                if (piece.color === myColor) {
+                    myScore += pieceValues[piece.type]
+                }
+                else {
+                    oppScore += pieceValues[piece.type]
+                }
+            }
+        }
+    }
+
+    return (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON)
 }
