34df2ce7a43b9b7e97691a241932a810809aebd5
Latti31 <111042692+Latti31@users.noreply.github.com>
2022-12-16 23:19:39 +1100

    Added test of node expansion

--- Webpack/Nodes.js
+++ Webpack/Nodes.js
@@ -6,13 +6,28 @@ function getID() {
 }
 
 class Node {
-    constructor(id, board, parent) {
+    constructor(id, board, parent, moves) {
+        this.id = id;
+        this.board = board;
+        this.parent = parent;
+        this.moves = moves;
+        this.children = []
+    }
+
+    fullyExplored() {
+        if (this.children.length === this.moves.length) return true
+        return false
+    }
+
+    // Multi - arm bandit
+    getNext() {
         
     }
+
 }
 
-exports.getNewNode = function getNode(board, parent) {
+exports.getNewNode = function getNode(board, parent, moves) {
 
-    var node = new Node(getID(), board, parent)
+    var node = new Node(getID(), board, parent, moves)
     return node
 }
