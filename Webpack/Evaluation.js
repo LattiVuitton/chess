029311408340d08c724b84bf7f86ceb8cba776a4
Latti31 <111042692+Latti31@users.noreply.github.com>
2022-12-24 23:48:42 +1100

    bug change

--- Webpack/Evaluation.js
+++ Webpack/Evaluation.js
@@ -10,15 +10,24 @@ const pieceValues = {
 
 exports.evaluateBoard = function evaluate(game) {
 
-    // Artificial Cost
-    var count = 0
-    for (let i = 0; i < 1000; i++){
-        denominator = Math.random()
-        count += (denominator - 0.5)
-    }
+
 
     // Evaluation
     return count
+}
+var highest = 0
+function artificialCost(cost) {
+    // Artificial Cost
+    var count = 0
+    for (let i = 0; i < cost; i++){
+        denominator = Math.random()
+        count += (denominator - 0.5)
+    }
+    
+    if (1 / count > 100000) {
+        console.log("A miracle: " + (1/count))
+    }
+
 }
 
 exports.pieceValue = function countPieces(game, myColor) {
@@ -29,6 +38,7 @@ exports.pieceValue = function countPieces(game, myColor) {
     var tilesList = []
     for (let i = 0; i < letters.length; i++){
         for (let j = 1; j < letters.length + 1; j++){
+            artificialCost(1000)
             tile = letters[i] + j
             tilesList.push(tile)
             piece = game.get(tile)
