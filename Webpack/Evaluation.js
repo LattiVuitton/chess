bf516826967b3ed60d76411b691aaa0c383797c4
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-01-11 22:49:53 +1100

    Updated immediate empty return for efficiency

--- Webpack/Evaluation.js
+++ Webpack/Evaluation.js
@@ -8,6 +8,8 @@ const pieceValues = {
     'k': 0
 }
 
+var boardsEvaluated = 0;
+
 exports.evaluateBoard = function evaluate(game) {
 
     // Evaluation
@@ -46,6 +48,8 @@ exports.pieceValue = function countPieces(game, nodeColor, action, preEval) {
         return preEval
     }
     
+    console.log(game.fen())
+
     var myScore = 0
     var oppScore = 0
 
@@ -73,5 +77,57 @@ exports.pieceValue = function countPieces(game, nodeColor, action, preEval) {
         }
     }
     // console.log(nodeColor + ": " + (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON))
+    console.log("ME: " + myScore + " them: " + oppScore)
     return (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON)
+}
+
+exports.getCount = function () {
+    return boardsEvaluated
+}
+
+// Used for light nodes
+exports.getQValue = function evaluateBoard(game, action, preEval) {
+
+    boardsEvaluated++
+
+    var pieceTaken = false;
+
+    if (action != null) {
+        stringSan = action.split("")
+        for (let i = 1; i < stringSan.length; i++){
+            if (stringSan[i] === 'x') {
+                pieceTaken = true;
+                break;
+            }
+        }
+    }
+
+    if (!pieceTaken && preEval >= 0 && preEval <= 1) {
+        return preEval
+    }
+    
+    var whiteScore = 0
+    var blackScore = 0
+
+    var tilesList = []
+    for (let i = 0; i < letters.length; i++){
+        for (let j = 1; j < letters.length + 1; j++){
+            // artificialCost(1000)
+            tile = letters[i] + j
+            tilesList.push(tile)
+            piece = game.get(tile)
+            if (piece != false) {
+                if (piece.color === 'w') {
+                    whiteScore += pieceValues[piece.type]
+                }
+                else {
+                    blackScore += pieceValues[piece.type]
+                }
+            }
+        }
+    }
+    // console.log(nodeColor + ": " + (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON))
+    // console.log("ME: " + whiteScore + " them: " + blackScore)
+    return (whiteScore / (whiteScore + blackScore))
+    return (whiteScore + Number.EPSILON) / (whiteScore + blackScore + Number.EPSILON)
 }
