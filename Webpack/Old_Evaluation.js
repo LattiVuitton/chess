010f53a05d0cb6cada669c6c91ce95e00ab40054
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-03-10 16:13:26 +1100

    Q-value backup

--- Webpack/Old_Evaluation.js
+++ Webpack/Old_Evaluation.js
@@ -70,3 +70,63 @@ exports.pieceValue = function countPieces(game, nodeColor, action, preEval) {
 exports.getCount = function () {
     return boardsEvaluated
 }
+
+
+// Used for light nodes
+exports.getQValue = function evaluateBoard(game, action, preEval, WorB) {
+
+    boardsEvaluated++
+
+    pieceTaken = false;
+
+    if (WorB) {
+        myColor = 'w';
+    }
+
+    else {
+        myColor = 'b';
+    }
+
+    if (action != null) {
+        stringSan = action.split("")
+        for (let i = 1; i < stringSan.length; i++){
+            if (stringSan[i] === 'x') {
+                pieceTaken = true;
+                break;
+            }
+        }
+    }
+
+    if (!pieceTaken && preEval >= 0 && preEval <= 1) {
+        return 1 - preEval
+    } 
+    
+    myScore = 0
+    opScore = 0
+
+    tilesList = []
+    for (let i = 0; i < letters.length; i++){
+        for (let j = 1; j < letters.length + 1; j++){
+            // artificialCost(1000)
+            tile = letters[i] + j
+            tilesList.push(tile)
+            piece = game.get(tile)
+            if (piece != false) {
+                if (piece.color === myColor) {
+                    myScore += pieceValues[piece.type]
+                }
+                else {
+                    opScore += pieceValues[piece.type]
+                }
+            }
+        }
+    }
+    // console.log(nodeColor + ": " + (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON))
+    // console.log("ME: " + whiteScore + " them: " + blackScore)
+    // return (myScore / (myScore + oppScore))
+    return (myScore + Number.EPSILON) / (myScore + opScore + Number.EPSILON)
+}
+
+exports.NN = function NeuralNet(game, action, preEval, WorB) {
+    return Math.random();
+}
