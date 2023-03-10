6b3fb7c8140dd359ab929565f6720cdf71ffcd31
Latti31 <111042692+Latti31@users.noreply.github.com>
2023-03-10 16:15:29 +1100

    Cleaning Code

--- Webpack/Evaluation.js
+++ Webpack/Evaluation.js
@@ -183,6 +183,7 @@ function swapNumber(rowNumber) {
     return 8 - rowNumber
 }
 
+// Gets piece value
 function getPieceValue(letter, number, type, color, endGame) {
     if (type === 'k') {
 
@@ -221,82 +222,7 @@ const pieceValues = {
     'q': 9.5,
     'k': 100
 }
-
-var boardsEvaluated = 0;
-
 var isEarlyGame = true;
-
-exports.evaluateBoard = function evaluate(game) {
-
-    // Evaluation
-    return -2
-}
-
-var highest = 0
-function artificialCost(cost) {
-    // Artificial Cost
-    var count = 0
-    for (let i = 0; i < cost; i++){
-        denominator = Math.random()
-        count += (denominator - 0.5)
-    }
-    
-    if (1 / count > 100000) {
-        console.log("A miracle: " + (1/count))
-    }
-}
-
-exports.pieceValue = function countPieces(game, nodeColor, action, preEval) {
-
-    var pieceTaken = false;
-
-    if (action != null) {
-        stringSan = action.san.split("")
-        for (let i = 0; i < stringSan.length; i++){
-            if (stringSan[i] === 'x') {
-                pieceTaken = true;
-            }
-        }
-    }
-
-    if (!pieceTaken && preEval >= 0 && preEval <= 1) {
-        return preEval
-    }
-    
-    var myScore = 0
-    var oppScore = 0
-
-    // myColor = 'b'
-    // if (nodeColor === 'b') {
-    //     myColor = 'w'
-    // }
-    var myColor = nodeColor
-
-    var tilesList = []
-    for (let i = 0; i < letters.length; i++){
-        for (let j = 1; j < letters.length + 1; j++){
-            // artificialCost(1000)
-            tile = letters[i] + j
-            tilesList.push(tile)
-            piece = game.get(tile)
-            if (piece != false) {
-                if (piece.color === myColor) {
-                    myScore += pieceValues[piece.type]
-                }
-                else {
-                    oppScore += pieceValues[piece.type]
-                }
-            }
-        }
-    }
-    // console.log(nodeColor + ": " + (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON))
-    console.log("ME: " + myScore + " them: " + oppScore)
-    return (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON)
-}
-
-exports.getCount = function () {
-    return boardsEvaluated
-}
 
 var pieceTaken = false;
 var myColor = 'w';
@@ -305,64 +231,7 @@ var valueScore = 0
 
 var tilesList = []
 
-// Used for light nodes
-exports.getQValue = function evaluateBoard(game, action, preEval, WorB) {
 
-    boardsEvaluated++
-
-    pieceTaken = false;
-
-    if (WorB) {
-        myColor = 'w';
-    }
-
-    else {
-        myColor = 'b';
-    }
-
-    if (action != null) {
-        stringSan = action.split("")
-        for (let i = 1; i < stringSan.length; i++){
-            if (stringSan[i] === 'x') {
-                pieceTaken = true;
-                break;
-            }
-        }
-    }
-
-    if (!pieceTaken && preEval >= 0 && preEval <= 1) {
-        return 1 - preEval
-    } 
-    
-    myScore = 0
-    opScore = 0
-
-    tilesList = []
-    for (let i = 0; i < letters.length; i++){
-        for (let j = 1; j < letters.length + 1; j++){
-            // artificialCost(1000)
-            tile = letters[i] + j
-            tilesList.push(tile)
-            piece = game.get(tile)
-            if (piece != false) {
-                if (piece.color === myColor) {
-                    myScore += pieceValues[piece.type]
-                }
-                else {
-                    opScore += pieceValues[piece.type]
-                }
-            }
-        }
-    }
-    // console.log(nodeColor + ": " + (myScore + Number.EPSILON) / (myScore + oppScore + Number.EPSILON))
-    // console.log("ME: " + whiteScore + " them: " + blackScore)
-    // return (myScore / (myScore + oppScore))
-    return (myScore + Number.EPSILON) / (myScore + opScore + Number.EPSILON)
-}
-
-exports.NN = function NeuralNet(game, action, preEval, WorB) {
-    return Math.random();
-}
 
 let positionScore = 0
 
